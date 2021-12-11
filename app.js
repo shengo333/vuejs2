531e7cdd6b6c33b5a19f22034c0ecfb36164b7b6
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-11 16:43:55 +0400

    two way data binding scratches

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 new Vue ({
     el: '#vue-app',
     data: {
-        age: 25,
-        x: 0,
-        y: 0
+        // age: 25,
+        // x: 0,
+        // y: 0
+        name: '',
+        age: '',
     },
     methods: {
-        subtract(dec){
-            this.age-= dec;
+
+        logName(){
+            console.log('you clicked alt+enter')
         },
-        add(inc){
-            this.age+= inc;
-        },
+        logAge(){
+            console.log('you clicked alt+enter')
+        }
+        // subtract(dec){
+        //     this.age-= dec;
+        // },
+        // add(inc){
+        //     this.age+= inc;
+        // },
         // addTen(){
         //     this.age+=10
         // },
         // subtractTen(){
         //     this.age-=10
         // }
-        updateXY(event){
-            console.log(event)
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click(){
-            alert('you clicked me')
-        }
+        // updateXY(event){
+        //     console.log(event)
+        //     this.x = event.offsetX;
+        //     this.y = event.offsetY;
+        // },
+        // click(){
+        //     alert('you clicked me')
+        // }
     }
 });
