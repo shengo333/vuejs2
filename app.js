19ef8ffa6dc9d4a3f6df33498c0e9e45b43d8b8f
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-11 12:36:54 +0400

    mousemove event is done

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 new Vue ({
     el: '#vue-app',
     data: {
-        age: 25
+        age: 25,
+        x: 0,
+        y: 0
     },
     methods: {
-
+        subtract(dec){
+            this.age-= dec;
+        },
+        add(inc){
+            this.age+= inc;
+        },
+        // addTen(){
+        //     this.age+=10
+        // },
+        // subtractTen(){
+        //     this.age-=10
+        // }
+        updateXY(event){
+            console.log(event)
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
     }
 });
